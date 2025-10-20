7b29899934451c5a85087828ba8b44c72fc20faa
Vu Minh Quan <quan2082003@gmail.com>
2025-10-20 15:44:02 +0700

    add

--- src/lib/db/mongodb.ts
+++ src/lib/db/mongodb.ts
@@ -1,34 +1,34 @@
-// /lib/db/connect.ts
-import mongoose from "mongoose";
+// // /lib/db/connect.ts
+// import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
+// const MONGODB_URI = process.env.MONGODB_URI as string;
 
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
-}
+// if (!MONGODB_URI) {
+//   throw new Error("Please define the MONGODB_URI environment variable");
+// }
 
-const globalWithMongoose = global as typeof globalThis & {
-  mongoose?: {
-    conn: typeof mongoose | null;
-    promise: Promise<typeof mongoose> | null;
-  };
-};
+// const globalWithMongoose = global as typeof globalThis & {
+//   mongoose?: {
+//     conn: typeof mongoose | null;
+//     promise: Promise<typeof mongoose> | null;
+//   };
+// };
 
-const cached = globalWithMongoose.mongoose || { conn: null, promise: null };
+// const cached = globalWithMongoose.mongoose || { conn: null, promise: null };
 
-export async function connectDB() {
-  if (cached.conn) return cached.conn;
+// export async function connectDB() {
+//   if (cached.conn) return cached.conn;
 
-  if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(MONGODB_URI, {
-        bufferCommands: false,
-      })
-      .then((mongoose) => {
-        return mongoose;
-      });
-  }
+//   if (!cached.promise) {
+//     cached.promise = mongoose
+//       .connect(MONGODB_URI, {
+//         bufferCommands: false,
+//       })
+//       .then((mongoose) => {
+//         return mongoose;
+//       });
+//   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
